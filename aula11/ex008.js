f32e434d43ec6bd6e659cd0d06eb27ead67ca37c
Jaison Costa <jaison.clementino@gmail.com>
2023-07-06 22:15:05 -0300

    Update Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/l`)
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
